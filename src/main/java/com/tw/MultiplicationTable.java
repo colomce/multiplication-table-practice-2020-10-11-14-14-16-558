1fb0a2b079577b5dce49fb09bdfba4a375d0f4d3
Cedric Don Coloma <colomce@oocl.com>
2020-10-13 16:12:00 +0800

    [Context Map][Cedric Coloma] feature: Added multplication table based on Context map created.

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,42 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
-        return null;
+    public String create(int firstNumber, int secondNumber) {
+        boolean isValid = isValid(firstNumber, secondNumber);
+        String multiplicationTable = buildMultiplicationTable(firstNumber, secondNumber);
+
+        return isValid ? multiplicationTable : null;
     }
+
+    private String buildMultiplicationTable(int firstNumber, int secondNumber) {
+        StringBuilder multiplicationTable = new StringBuilder();
+        for(int multiplier = firstNumber; multiplier <= secondNumber; multiplier++){
+            for(int multplicand = firstNumber; multplicand <= multiplier; multplicand++){
+                multiplicationTable.append(multplicand)
+                        .append("*")
+                        .append(multiplier)
+                        .append("=")
+                        .append(multiplier * multplicand)
+                        .append(" ");
+            }
+            multiplicationTable.append("\n");
+        }
+        return multiplicationTable.toString();
+    }
+
+    private boolean isValid(int firstNumber, int secondNumber) {
+        boolean isFirstNumberSmaller = isFirstNumberSmallerOrEqual(firstNumber, secondNumber);
+        boolean allNumbersInRange = isInRange(1, 1000, firstNumber) && isInRange(1, 1000, secondNumber);
+
+        return isFirstNumberSmaller && allNumbersInRange;
+    }
+
+    private boolean isInRange(int min, int max, int number) {
+        return number >= min && number <= max;
+    }
+
+    private boolean isFirstNumberSmallerOrEqual(int firstNumber, int secondNumber) {
+        return firstNumber <= secondNumber;
+    }
+
 }
